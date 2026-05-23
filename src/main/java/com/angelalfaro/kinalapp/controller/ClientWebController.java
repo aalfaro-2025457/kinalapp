75e2984eccbeec72b70a94b3836a9c8009cf70ea
aalfaro-2025457 <aalfaro-2025457@kinal.edu.gt>
2026-05-23 09:41:22 -0600

    refactor: showing errors in Clients view

--- src/main/java/com/angelalfaro/kinalapp/controller/ClientWebController.java
+++ src/main/java/com/angelalfaro/kinalapp/controller/ClientWebController.java
@@ -3,6 +3,8 @@ package com.angelalfaro.kinalapp.controller;
 import com.angelalfaro.kinalapp.entity.Client;
 import com.angelalfaro.kinalapp.service.client.ClientService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -28,43 +30,89 @@ public class ClientWebController {
     }
 
     @PostMapping("/save")
-    public String saveClient(@ModelAttribute("newClient") Client client) {
-        clientService.save(client);
-        return "redirect:/view/clients";
+    public String saveClient(@ModelAttribute("newClient") Client client, Model model) {
+        try {
+            clientService.save(client);
+            return "redirect:/view/clients";
+        } catch (Exception e){
+            List<Client> clients = clientService.listAll();
+            model.addAttribute("clients", clients);
+            model.addAttribute("newClient", new Client());
+            model.addAttribute("errorMsg", "No tienes el rol de ADMINISTRADOR para realizar esta acción.");
+            
+            return "cruds/clients";
+        }
     }
 
     @GetMapping("/edit/{dpi}")
     public String editClient(@PathVariable String dpi, Model model) {
-        // Find the Client by dpi
-        Optional<Client> clientToEdit = clientService.findByDPI(dpi);
+        try {
+            // Find the Client by dpi
+            Optional<Client> clientToEdit = clientService.findByDPI(dpi);
 
-        if (clientToEdit.isPresent()) {
-            model.addAttribute("clients", clientService.listAll());
+            if (clientToEdit.isPresent()) {
+                model.addAttribute("clients", clientService.listAll());
 
-            model.addAttribute("newClient", clientToEdit.get());
+                model.addAttribute("newClient", clientToEdit.get());
+                return "cruds/clients";
+            }
+            return "redirect:/view/clients";
+        } catch (Exception e){
+            List<Client> clients = clientService.listAll();
+            model.addAttribute("clients", clients);
+            model.addAttribute("newClient", new Client());
+            model.addAttribute("errorMsg", "No tienes el rol de ADMINISTRADOR para realizar esta acción.");
+            
             return "cruds/clients";
         }
-        return "redirect:/view/clients";
     }
 
     @GetMapping("/delete/{dpi}")
-    public String deleteClient(@PathVariable String dpi) {
-        clientService.delete(dpi);
-        return "redirect:/view/clients";
+    public String deleteClient(@PathVariable String dpi, Model model) {
+        try {
+            clientService.delete(dpi);
+            return "redirect:/view/clients";
+        } catch (Exception e){
+            List<Client> clients = clientService.listAll();
+            model.addAttribute("clients", clients);
+            model.addAttribute("newClient", new Client());
+            model.addAttribute("errorMsg", "No tienes el rol de ADMINISTRADOR para realizar esta acción.");
+            
+            return "cruds/clients";
+        }
     }
 
     @GetMapping("/search")
     public String searchByDpi(@RequestParam(name = "dpiSearch", required = false) String dpiSearch, Model model) {
-        List<Client> results = new ArrayList<>();
+        try {
+            List<Client> results = new ArrayList<>();
 
-        if (dpiSearch != null && !dpiSearch.trim().isEmpty()) {
-            clientService.findByDPI(dpiSearch).ifPresent(results::add);
-        } else {
-            results = clientService.listAll();
+            if (dpiSearch != null && !dpiSearch.trim().isEmpty()) {
+                clientService.findByDPI(dpiSearch).ifPresent(results::add);
+            } else {
+                results = clientService.listAll();
+            }
+
+            model.addAttribute("clients", results);
+            model.addAttribute("newClient", new Client());
+            return "cruds/clients";
+        } catch (Exception e){
+            List<Client> clients = clientService.listAll();
+            model.addAttribute("clients", clients);
+            model.addAttribute("newClient", new Client());
+            model.addAttribute("errorMsg", "No tienes el rol de ADMINISTRADOR para realizar esta acción.");
+            
+            return "cruds/clients";
         }
+    }
 
-        model.addAttribute("clients", results);
+    @ExceptionHandler(AccessDeniedException.class)
+    public String handleAccessDenied(AccessDeniedException ex, Model model) {
+        List<Client> clients = clientService.listAll();
+        model.addAttribute("clients", clients);
         model.addAttribute("newClient", new Client());
+        model.addAttribute("errorMsg", "No tienes el rol de ADMINISTRADOR para realizar esta acción.");
+        
         return "cruds/clients";
     }
 
